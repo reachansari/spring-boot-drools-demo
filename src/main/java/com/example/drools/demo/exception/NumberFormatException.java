045d6b373f799ac55225958f92316505e82fab50
Thameem <reachansari@gmail.com>
2021-10-06 21:56:03 +0800

    first commit

---
+++ src/main/java/com/example/drools/demo/exception/NumberFormatException.java
@@ -0,0 +1,11 @@
+package com.example.drools.demo.exception;
+
+public class NumberFormatException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public NumberFormatException(String msg) {
+		super(msg);
+	}
+
+}
